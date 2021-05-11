99257994c8d6e6da7a51897ac67d645bb14fedd5
19011768 <rayhaannak1@gmail.com>
2021-05-11 13:51:13 +0200

    SettingsActivity Updated

--- app/src/main/java/com/sm19003564/omnimarkerza/SettingsActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/SettingsActivity.java
@@ -3,11 +3,13 @@ package com.sm19003564.omnimarkerza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.Switch;
 
 public class SettingsActivity extends AppCompatActivity {
 
     Switch switchSettings;
+    Button btnAbout, btnHelp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +18,8 @@ public class SettingsActivity extends AppCompatActivity {
 
         switchSettings = findViewById(R.id.switchSettings);
 
-  
+        btnAbout = findViewById(R.id.btnAbout);
+        btnHelp = findViewById(R.id.btnHelp);
 
     }
 }
