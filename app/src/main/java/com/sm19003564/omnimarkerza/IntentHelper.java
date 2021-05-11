c3d431eb91eaec8e0ca4560b017de9ecb8c79262
Suvashin Moodliar <suvashins@gmail.com>
2021-05-11 11:13:03 +0200

    Added some j code to logins

---
+++ app/src/main/java/com/sm19003564/omnimarkerza/IntentHelper.java
@@ -0,0 +1,13 @@
+package com.sm19003564.omnimarkerza;
+
+import android.content.Context;
+import android.content.Intent;
+
+public class IntentHelper {
+    public static void openIntent(Context context, Class passTo){
+
+        Intent i = new Intent(context, passTo);
+
+        context.startActivity(i);
+    }
+}
--- app/src/main/java/com/sm19003564/omnimarkerza/RegisterActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/RegisterActivity.java
@@ -4,14 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        //test
-
+        setContentView(R.layout.activity_register);
     }
 }
