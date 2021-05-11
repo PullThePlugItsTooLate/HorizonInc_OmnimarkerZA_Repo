1203a1c130026df89ee61d16f385482e95cb18db
19011768 <rayhaannak1@gmail.com>
2021-05-11 11:10:21 +0200

    Menu Activity Updated

--- app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
@@ -3,12 +3,29 @@ package com.sm19003564.omnimarkerza;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button landmarks, favourites, settings, safetyInfo, currencyConverter, weather, profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        landmarks = findViewById(R.id.btnMenuLandmarks);
+        favourites = findViewById(R.id.btnMenuFavourite);
+        settings = findViewById(R.id.btnMenuSettings);
+        safetyInfo = findViewById(R.id.btnMenuSafetyInfo);
+        currencyConverter = findViewById(R.id.btnMenuCurrency);
+        weather = findViewById(R.id.btnMenuWeather);
+        profile = findViewById(R.id.btnMenuProfile);
+
+
+
+
+
+
     }
 }
