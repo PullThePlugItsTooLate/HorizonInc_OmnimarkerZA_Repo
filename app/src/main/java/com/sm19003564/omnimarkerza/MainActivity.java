8c534360d0c03ac059fc09edc6cf6dc845a807e2
19011768 <rayhaannak1@gmail.com>
2021-05-11 11:22:47 +0200

    Settings Activity Added

--- app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
@@ -2,30 +2,35 @@ package com.sm19003564.omnimarkerza;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button landmarks, favourites, settings, safetyInfo, currencyConverter, weather, profile;
+    Button btnLandmarks, btnFavourites, btnSettings, btnSafetyInfo, btnCurrencyConverter, btnWeather, btnProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        landmarks = findViewById(R.id.btnMenuLandmarks);
-        favourites = findViewById(R.id.btnMenuFavourite);
-        settings = findViewById(R.id.btnMenuSettings);
-        safetyInfo = findViewById(R.id.btnMenuSafetyInfo);
-        currencyConverter = findViewById(R.id.btnMenuCurrency);
-        weather = findViewById(R.id.btnMenuWeather);
-        profile = findViewById(R.id.btnMenuProfile);
+        btnLandmarks = findViewById(R.id.btnMenuLandmarks);
+        btnFavourites = findViewById(R.id.btnMenuFavourite);
+        btnSettings = findViewById(R.id.btnMenuSettings);
+        btnSafetyInfo = findViewById(R.id.btnMenuSafetyInfo);
+        btnCurrencyConverter = findViewById(R.id.btnMenuCurrency);
+        btnWeather = findViewById(R.id.btnMenuWeather);
+        btnProfile = findViewById(R.id.btnMenuProfile);
 
-
-
-
-
+        btnLandmarks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, SettingsActivity.class);
+                startActivity(i);
+            }
+        });
 
     }
 }
