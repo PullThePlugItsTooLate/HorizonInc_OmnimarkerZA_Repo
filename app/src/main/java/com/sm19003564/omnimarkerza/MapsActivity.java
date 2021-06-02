0c4c5c4bfdc43d4df3a43283d69edf77a8fc09de
Rayhaan <rayhaannak1@gmail.com>
2021-06-02 19:36:28 +0200

    Location Permissions Added

--- app/src/main/java/com/sm19003564/omnimarkerza/MapsActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/MapsActivity.java
@@ -1,7 +1,13 @@
 package com.sm19003564.omnimarkerza;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
 import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -15,25 +21,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
 
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    private Boolean mLocationPermissionsGranted = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        getLocationPermission();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -42,5 +48,49 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng durban = new LatLng(-29.8587, 31.0218);
         mMap.addMarker(new MarkerOptions().position(durban).title("Marker in Durban"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(durban, 15f));
+    }
+
+
+    private void getLocationPermission() {
+        String[] permissions = {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            if(ContextCompat.checkSelfPermission(this.getApplicationContext(),
+                    COURSE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        permissions,
+                        LOCATION_PERMISSION_REQUEST_CODE);
+            }
+            }
+        else {
+            ActivityCompat.requestPermissions(this,
+                    permissions,
+                    LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+       mLocationPermissionsGranted = false;
+
+       switch(requestCode){
+           case LOCATION_PERMISSION_REQUEST_CODE:{
+               if(grantResults.length > 0){
+                   for (int i = 0; i < grantResults.length; i++){
+                       if(grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                           mLocationPermissionsGranted = false;
+                           return;
+                       }
+                   }
+                   mLocationPermissionsGranted = true;
+
+
+               }
+           }
+       }
     }
 }
