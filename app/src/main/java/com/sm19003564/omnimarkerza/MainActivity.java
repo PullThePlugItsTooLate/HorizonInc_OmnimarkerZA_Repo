c415622e8e40f5b3f7311734b51f809dd8bd80d6
Suvashin Moodliar <suvashins@gmail.com>
2021-06-03 15:16:28 +0200

    Saves settings to db, displays as text in main menu

--- app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/MainActivity.java
@@ -1,15 +1,30 @@
 package com.sm19003564.omnimarkerza;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btnLandmarks, btnFavourites, btnSettings, btnSafetyInfo, btnCurrencyConverter, btnWeather, btnProfile;
+    TextView tvMeasure;
+    Settings settings;
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference settingsRef = database.getReference(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +38,30 @@ public class MainActivity extends AppCompatActivity {
         btnCurrencyConverter = findViewById(R.id.btnMenuCurrency);
         btnWeather = findViewById(R.id.btnMenuWeather);
         btnProfile = findViewById(R.id.btnMenuProfile);
+        tvMeasure = findViewById(R.id.tvMeasure);
+        settings = new Settings();
+
+
+        settingsRef.child("SettingsData").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataInfo: snapshot.getChildren()){
+                    settings = dataInfo.getValue(Settings.class);
+                }
+                if (settings.isMetric()){
+                    tvMeasure.setText("Metric");
+                }else{
+                    if (settings.isImperial()){
+                        tvMeasure.setText("Imperial");
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         btnSettings.setOnClickListener(new View.OnClickListener() {
             @Override
