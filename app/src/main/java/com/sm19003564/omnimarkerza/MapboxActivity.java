7b006f812e24e84950e3f99314e72bbacd3b5a22
Suvashin Moodliar <suvashins@gmail.com>
2021-06-24 01:06:45 +0200

    Search feature added

--- app/src/main/java/com/sm19003564/omnimarkerza/MapboxActivity.java
+++ app/src/main/java/com/sm19003564/omnimarkerza/MapboxActivity.java
@@ -1,6 +1,9 @@
 package com.sm19003564.omnimarkerza;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Bundle;
 import java.util.List;
 import androidx.annotation.NonNull;
@@ -14,8 +17,13 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.gson.JsonObject;
 import com.mapbox.api.directions.v5.DirectionsCriteria;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.geojson.FeatureCollection;
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.Style;
@@ -31,11 +39,14 @@ import com.mapbox.mapboxsdk.location.modes.CameraMode;
 import com.mapbox.geojson.Feature;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.PlaceAutocomplete;
+import com.mapbox.mapboxsdk.plugins.places.autocomplete.model.PlaceOptions;
 import com.mapbox.mapboxsdk.style.layers.SymbolLayer;
 import com.mapbox.mapboxsdk.style.sources.GeoJsonSource;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconAllowOverlap;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconIgnorePlacement;
 import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconImage;
+import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.iconOffset;
 
 // classes to calculate a route
 import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions;
@@ -73,6 +84,13 @@ public class MapboxActivity extends AppCompatActivity implements OnMapReadyCallb
     private String measure;
     FirebaseDatabase database = FirebaseDatabase.getInstance();
     DatabaseReference settingsRef = database.getReference(FirebaseAuth.getInstance().getCurrentUser().getUid());
+
+    //Places
+    private static final int REQUEST_CODE_AUTOCOMPLETE = 1;
+    private CarmenFeature hospital;
+    private CarmenFeature college;
+    private String geojsonSourceLayerId = "geojsonSourceLayerId";
+    private String symbolIconId = "symbolIconId";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -115,6 +133,20 @@ public class MapboxActivity extends AppCompatActivity implements OnMapReadyCallb
 
                 addDestinationIconSymbolLayer(style);
 
+                initSearchFab();
+
+                addUserLocations();
+
+                // Add the symbol layer icon to map for future use
+                style.addImage(symbolIconId, BitmapFactory.decodeResource(
+                        MapboxActivity.this.getResources(), R.drawable.blue_picker3));
+
+                // Create an empty GeoJSON source using the empty feature collection
+                setUpSource(style);
+
+                // Set up a new symbol layer for displaying the searched location's feature coordinates
+                setupLayer(style);
+
                 mapboxMap.addOnMapClickListener(MapboxActivity.this);
                 button = findViewById(R.id.startButton);
                 button.setOnClickListener(new View.OnClickListener() {
@@ -131,6 +163,83 @@ public class MapboxActivity extends AppCompatActivity implements OnMapReadyCallb
                 });
             }
         });
+    }
+
+    private void initSearchFab() {
+        findViewById(R.id.fab_location_search).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new PlaceAutocomplete.IntentBuilder()
+                        .accessToken(Mapbox.getAccessToken() != null ? Mapbox.getAccessToken() : getString(R.string.access_token))
+                        .placeOptions(PlaceOptions.builder()
+                                .backgroundColor(Color.parseColor("#EEEEEE"))
+                                .limit(10)
+                                .addInjectedFeature(hospital)
+                                .addInjectedFeature(college)
+                                .build(PlaceOptions.MODE_CARDS))
+                        .build(MapboxActivity.this);
+                startActivityForResult(intent, REQUEST_CODE_AUTOCOMPLETE);
+            }
+        });
+    }
+
+    private void addUserLocations() {
+        hospital = CarmenFeature.builder().text("Core Medical Centre")
+                .geometry(Point.fromLngLat(31.030243117108757, -29.794697509199565))
+                .placeName("20 Gainsborough Dr, Athlone, Durban North, 4051")
+                .id("core-med-cen")
+                .properties(new JsonObject())
+                .build();
+
+        college = CarmenFeature.builder().text("The IIE's Varsity College - Durban North")
+                .placeName("12 Radar Dr, Durban North, Durban, 4051")
+                .geometry(Point.fromLngLat(31.035638157986508, -29.79661356385285))
+                .id("iie-vc-dbn")
+                .properties(new JsonObject())
+                .build();
+    }
+
+    private void setUpSource(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addSource(new GeoJsonSource(geojsonSourceLayerId));
+    }
+
+    private void setupLayer(@NonNull Style loadedMapStyle) {
+        loadedMapStyle.addLayer(new SymbolLayer("SYMBOL_LAYER_ID", geojsonSourceLayerId).withProperties(
+                iconImage(symbolIconId),
+                iconOffset(new Float[] {0f, -8f})
+        ));
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_AUTOCOMPLETE) {
+
+            // Retrieve selected location's CarmenFeature
+            CarmenFeature selectedCarmenFeature = PlaceAutocomplete.getPlace(data);
+
+            // Create a new FeatureCollection and add a new Feature to it using selectedCarmenFeature above.
+            // Then retrieve and update the source designated for showing a selected location's symbol layer icon
+
+            if (mapboxMap != null) {
+                Style style = mapboxMap.getStyle();
+                if (style != null) {
+                    GeoJsonSource source = style.getSourceAs(geojsonSourceLayerId);
+                    if (source != null) {
+                        source.setGeoJson(FeatureCollection.fromFeatures(
+                                new Feature[] {Feature.fromJson(selectedCarmenFeature.toJson())}));
+                    }
+
+                    // Move map camera to the selected location
+                    mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(
+                            new CameraPosition.Builder()
+                                    .target(new LatLng(((Point) selectedCarmenFeature.geometry()).latitude(),
+                                            ((Point) selectedCarmenFeature.geometry()).longitude()))
+                                    .zoom(14)
+                                    .build()), 4000);
+                }
+            }
+        }
     }
 
     private void addDestinationIconSymbolLayer(@NonNull Style loadedMapStyle) {
